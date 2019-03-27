77eff153be3d39841be83d0594121a4e10b8a362
bauuuu1021 <bboy010554@icloud.com>
2019-03-27 23:42:58 +0800

    Add additional mode - predict mode
    Predict mode can execute prediction using data from
    input_predict.cvs rather than testing set

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import argparse
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
-def main():
-    df = pd.read_csv('data/input.csv')
+def main(mode):
+    df = pd.read_csv('data/input_train.csv')
     data_other = df.drop(columns=['peak_load','index','Taipei','newTaipei','taichung',\
     'kaohsiung' ,'taoyuan',	'tainan', 'changhua', 'pingtung', 'yunlin', 'hsinchu'],axis=1)
     data_peak  = df['peak_load']
@@ -18,26 +19,38 @@ def main():
     train_other = sc.fit_transform(train_other)
     test_other = sc.fit_transform(test_other)
     
-    # add classifier, train, and predict
+    # add classifier and train model
     support_vector_classifier = SVC(kernel='rbf')
     support_vector_classifier.fit(train_other,train_peak)
-    predict_peak = support_vector_classifier.predict(test_other)
 
-    # calculate error
-    # percentage deviation : arithmetic mean
-    # average deviation of testing set : RMSE 
-    test_peakList = test_peak.tolist()
-    sumRate = 0
-    sumRMSE = 0
-    for i in range(len(test_peakList)):
-        dist = abs(predict_peak[i]-test_peakList[i])
-        errRate = dist/test_peakList[i]
-        sumRate += errRate
-        sumRMSE += dist*dist
+    if (mode == "train"):
+        # predict
+        predict_peak = support_vector_classifier.predict(test_other)
+
+        # calculate error
+        # percentage deviation : arithmetic mean
+        # average deviation of testing set : RMSE 
+        test_peakList = test_peak.tolist()
+        sumRate = 0
+        sumRMSE = 0
+        for i in range(len(test_peakList)):
+            dist = abs(predict_peak[i]-test_peakList[i])
+            errRate = dist/test_peakList[i]
+            sumRate += errRate
+            sumRMSE += dist*dist
+        
+        print('\n\npercentage\t', sumRate/len(test_peakList))
+        print('RMSE\t\t', (sumRMSE/len(test_peakList))**(1/2))
     
-    print('--------------------')
-    print('percentage\t', sumRate/len(test_peakList))
-    print('RMSE\t\t', (sumRMSE/len(test_peakList))**(1/2))
+    else:
+        input_predict = pd.read_csv('data/input_predict.csv')
+        input_predict = input_predict.drop(columns=['peak_load','index','Taipei','newTaipei','taichung',\
+        'kaohsiung' ,'taoyuan',	'tainan', 'changhua', 'pingtung', 'yunlin', 'hsinchu'],axis=1)
+        predict_peak = support_vector_classifier.predict(input_predict)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', default='predict', 
+    help='choose training mode (measure accuracy) or predicting mode (by input_predict.csv), default predict')
+    args = parser.parse_args()
+    main(args.mode)
